457b935db053ed087c0e9e41a59ff26024a7cd10
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-02-18 12:05:21 -0500

    lesson done

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/dojo-logo.png";
 
 export default function Navbar() {
   return (
     <nav>
+      <Image
+        src={logo}
+        alt={`Dojo Helpdesk Logo`}
+        width={72}
+        quality={100}
+        placeholder={`empty`}
+      />
+
       <h1>Net Ninja Next Tutorial</h1>
       <Link href={`/`}>Dashboard</Link>
       <Link href={`/tickets`}>Tickets</Link>
